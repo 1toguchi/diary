556812e0ae905b360c8201af8f96c29c754b266c
ytksy <ytksy1218@gmail.com>
2016-08-11 23:43:19 +0900

    Update member.rb

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,7 @@ class Member < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  #mount_uploader :image, ImageUploaderまでスペースが一つ多い。       
    has_many :kijis, dependent: :destroy
    has_many :votes, dependent: :destroy
    has_many :voted_kijis, through: :votes, source: :kiji
@@ -11,9 +12,12 @@ class Member < ActiveRecord::Base
    validates :gender, presence: true
    mount_uploader :image, ImageUploader
 
+  #こんな感じでpresenceを揃えられるとgood
+  #validates :name,   presence: true, length: { minimum: 1, maximum: 50 }
+  #validates :email,  presence: true
+  #validates :gender, presence: true
 
   def votable_for?(kiji)
-    kiji && kiji.author != self && !votes.exists?(kiji_id:
-     kiji.id)
+    kiji && kiji.author != self && !votes.exists?(kiji_id: kiji.id)
   end
 end
